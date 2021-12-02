33ec2c01645d8dc6c1d8726cf6635846ffb5adef
Sebastian Vega <sebasvega4604@gmail.com>
2021-12-02 15:58:07 -0500

    useless commit

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,23 +9,23 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Drivetrain {
     // LEFT DRIVETRAIN
-    private final WPI_TalonSRX leftMoterController1= new WPI_TalonSRX(RobotMap.TALON_LEFT_DRIVE_PORTS[0]);
-    private final WPI_VictorSPX leftMoterController2= new WPI_VictorSPX(RobotMap.TALON_LEFT_DRIVE_PORTS[1]);
-    private final WPI_VictorSPX leftMoterController3= new WPI_VictorSPX(RobotMap.TALON_LEFT_DRIVE_PORTS[2]);
+    private final WPI_TalonSRX leftMoterController1 = new WPI_TalonSRX(RobotMap.TALON_LEFT_DRIVE_PORTS[0]);
+    private final WPI_VictorSPX leftMoterController2 = new WPI_VictorSPX(RobotMap.TALON_LEFT_DRIVE_PORTS[1]);
+    private final WPI_VictorSPX leftMoterController3 = new WPI_VictorSPX(RobotMap.TALON_LEFT_DRIVE_PORTS[2]);
 
     // RIGHT DRIVETRAIN
-    private final WPI_TalonSRX rightMoterController1= new WPI_TalonSRX(RobotMap.TALON_RIGHT_DRIVE_PORTS[0]);
-    private final WPI_VictorSPX rightMoterController2= new WPI_VictorSPX(RobotMap.TALON_RIGHT_DRIVE_PORTS[1]);
-    private final WPI_VictorSPX rightMoterController3= new WPI_VictorSPX(RobotMap.TALON_RIGHT_DRIVE_PORTS[2]);
+    private final WPI_TalonSRX rightMoterController1 = new WPI_TalonSRX(RobotMap.TALON_RIGHT_DRIVE_PORTS[0]);
+    private final WPI_VictorSPX rightMoterController2 = new WPI_VictorSPX(RobotMap.TALON_RIGHT_DRIVE_PORTS[1]);
+    private final WPI_VictorSPX rightMoterController3 = new WPI_VictorSPX(RobotMap.TALON_RIGHT_DRIVE_PORTS[2]);
 
     // Grouping controllers
 
-    private final SpeedControllerGroup lControllerGroup= new SpeedControllerGroup(leftMoterController1, leftMoterController2, leftMoterController3);
-    private final SpeedControllerGroup rControllerGroup= new SpeedControllerGroup(rightMoterController1, rightMoterController2, rightMoterController3);
+    private final SpeedControllerGroup lControllerGroup = new SpeedControllerGroup(leftMoterController1, leftMoterController2, leftMoterController3);
+    private final SpeedControllerGroup rControllerGroup = new SpeedControllerGroup(rightMoterController1, rightMoterController2, rightMoterController3);
 
     // Differential drive
 
-    private final DifferentialDrive drive= new DifferentialDrive(lControllerGroup, rControllerGroup);
+    private final DifferentialDrive drive = new DifferentialDrive(lControllerGroup, rControllerGroup);
 
     
 
