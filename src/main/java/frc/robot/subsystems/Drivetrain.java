b8a4b9bf7458f5f4d3da984ec0d437b4815861c0
bartekkowalski465 <bartekkowalski465@gmail.com>
2021-12-13 16:24:23 -0500

    test

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,8 +1,8 @@
 package frc.robot.subsystems;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+//import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-import edu.wpi.first.wpilibj.controller.PIDController;
+//import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -28,13 +28,13 @@ public class Drivetrain extends Subsystem{
     private final DifferentialDrive drive = new DifferentialDrive(lControllerGroup, rControllerGroup);
 
     // PID controller constant
-    static private final double kp = 2;
-    static private final double ki = 0;
-    static private final double kd = 0;
+    //static private final double kp = 2;
+    //static private final double ki = 0;
+    //static private final double kd = 0;
     
     // PID controllers
-    private final PIDController leftPidController = new PIDController(kp, ki, kd);
-    private final PIDController rightPidController = new PIDController(kp, ki, kd);
+    //private final PIDController leftPidController = new PIDController(kp, ki, kd);
+    //private final PIDController rightPidController = new PIDController(kp, ki, kd);
 
     
     public void drivetrain(double leftSpeed, double rightSpeed){
